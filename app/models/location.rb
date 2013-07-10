724d3225be6a0b5b386923d39217dccf9079bf84
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2013-07-10 09:49:48 -0300

    Adding searchability
    NO testing is making me sad

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,7 +1,6 @@
 class Location < ActiveRecord::Base
   attr_accessible :aliases, :feature_class, :latitude, :longitude, :preferred_names
 
-
   has_and_belongs_to_many :children,
     class_name: 'LocationRelationship',
     join_table: 'location_relationships',
@@ -13,4 +12,11 @@ class Location < ActiveRecord::Base
     join_table: 'location_relationships',
     foreign_key: 'child_location_id',
     association_foreign_key: 'parent_location_id'
+
+  searchable do
+    text(:preferred_names)
+    text(:aliases)
+    string(:feature_class)
+    latlon(:location) { Sunspot::Util::Coordinates.new(latitude, longitude) }
+  end
 end
