3299ebd5d6e72de91f43b38f8eede957f109da1d
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2013-07-10 09:41:55 -0300

    Updating location to include relationships

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,3 +1,16 @@
 class Location < ActiveRecord::Base
   attr_accessible :aliases, :feature_class, :latitude, :longitude, :preferred_names
+
+
+  has_and_belongs_to_many :children,
+    class_name: 'LocationRelationship',
+    join_table: 'location_relationships',
+    foreign_key: 'parent_location_id',
+    association_foreign_key: 'child_location_id'
+
+  has_and_belongs_to_many :parents,
+    class_name: 'LocationRelationship',
+    join_table: 'location_relationships',
+    foreign_key: 'child_location_id',
+    association_foreign_key: 'parent_location_id'
 end
