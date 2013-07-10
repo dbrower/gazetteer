f21a50fdc2fa55afec561fa92bb269621ae01db5
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2013-07-10 08:47:43 -0300

    Creating rails project
    $ rails new gazetteer

--- README.md
+++ README.md
@@ -1,4 +1,1 @@
-gazetteer
-=========
-
-Provide a gazetteer service for mapping place names to coordinates and vice-versa
+# Gazetteer
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gazetteer::Application.config.secret_token = '4e8994365f76126038c89641c7ef8f85fed940c7206bf82254674c1dce972e67df7984e0df02badc736649169d5aaba192d31d42f4cd7c391da8402b9d77d918'
