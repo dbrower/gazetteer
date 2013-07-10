eb033f717bdc24e954890cee32ad9717194f3c4b
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2013-07-10 09:40:16 -0300

    Creating table for HABTM

--- db/migrate/20130710122118_create_location_relationships.rb
+++ db/migrate/20130710122118_create_location_relationships.rb
@@ -1,6 +1,6 @@
 class CreateLocationRelationships < ActiveRecord::Migration
   def change
-    create_table :location_relationships do |t|
+    create_table :location_relationships, id: false do |t|
       t.integer :parent_location_id, index: true, null: false
       t.integer :child_location_id, index: true, null: false
 
